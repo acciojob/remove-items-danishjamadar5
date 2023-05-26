9caa24781f2dbdc239195ef7aa9a4eb97c0075c1
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-05-26 11:34:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function deleteElement() {
+     let selecctElement=document.getElementByTagName("select")[0];
+	let selectedValue=selectElement.value;
+	let childrenList=selectElement.children;
+
+	for(let i=0;i<childrenList;i++)
+		{
+			if(selectedValue===childrenList[i].value)
+			{
+				childrenList[i].remove();
+			}
+		}
+}
